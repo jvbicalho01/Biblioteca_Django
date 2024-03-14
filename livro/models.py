1b699ec5a3b2851c81a040f946c7ab98775e619b
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-03-14 16:16:02 -0300

    add table Livros in DB

--- livro/models.py
+++ livro/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Livros(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=30)
+    co_autor = models.CharField(max_length=30)
+    data_cadastro = models.DateField()
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=30)
+    data_emprestimo = models.DateTimeField()
+    data_devolução = models.DateTimeField()
+    tempo_duracao = models.DateField()
