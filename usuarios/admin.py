70757c6f7a37d1a91b83d03644d2f82c929a3909
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-03-28 14:46:47 -0300

    add model Usuario

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Usuario
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('nome', 'email', 'senha')
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Usuario
+
 def login(request):
     return HttpResponse('login')
 
@@ -11,5 +13,8 @@ def valida_cadastro(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
+
+    usuario = Usuario(nome=nome, email=email, senha=senha)
+    usuario.save()
 
     return HttpResponse(f'{nome} {email} {senha}')
