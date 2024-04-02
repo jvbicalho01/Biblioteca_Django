5d75cb42c447617a0c2b89431b101809fc85ae6a
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-04-02 13:44:51 -0300

    add listagem de emprestimos

--- livro/views.py
+++ livro/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from usuarios.models import Usuario
-from .models import Livros, Categoria
+from .models import Livros, Categoria, Emprestimo
 
 def home(request):
     if request.session.get('usuario'):
@@ -16,8 +16,9 @@ def ver_livro(request, id):
     if request.session.get('usuario'):
         livro = Livros.objects.get(id=id)
         if request.session.get('usuario') == livro.usuario.id:    
-            categoria_livro = Categoria.objects.filter(usuario_id=request.session.get('usuario'))    
-            return render(request, 'ver_livro.html', {'livro': livro, 'categoria_livro': categoria_livro})
+            categoria_livro = Categoria.objects.filter(usuario_id=request.session.get('usuario'))  
+            emprestimos = Emprestimo.objects.filter(livro=livro)
+            return render(request, 'ver_livro.html', {'livro': livro, 'categoria_livro': categoria_livro, 'emprestimos': emprestimos})
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('/auth/login/?status=2')
